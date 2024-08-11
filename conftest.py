70e6bbfd16cd0b4a1eeba913b0c26c5bf0466ae7
Alexander Baranov <dj-bars@yandex.ru>
2024-08-11 18:28:40 +0300

    Second commit

--- conftest.py
+++ conftest.py
@@ -18,3 +18,4 @@ def courier_delete():
     print(courier_create['data'])
     courier_login = Courier().courier_login_in_the_system_and_get_id_courier(courier_create["data"])
     yield courier_login
+    Courier().courier_subsequent_deletion(courier_login["id"])
